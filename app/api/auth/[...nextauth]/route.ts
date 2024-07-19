d124d6f446033aed07c370b68a36dbcd11ed38df
saikarthik.mudigonda@techwave.net <saikarthik.mudigonda@techwave.net>
2024-07-19 12:31:52 +0530

    build issue

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,23 @@
+// app/api/auth/[...nextauth]/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 
 const options = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-    ],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
 };
 
 const handler = NextAuth(options);
 
 export async function GET(req: NextRequest) {
-    return handler(req, NextResponse);
+  return handler(req, NextResponse);
 }
 
 export async function POST(req: NextRequest) {
-    return handler(req, NextResponse);
+  return handler(req, NextResponse);
 }
