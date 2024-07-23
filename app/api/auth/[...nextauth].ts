5b45648ef104c0112e8e63ebcc6b571ccf5ebd5b
sai karthik <84711965+saikarthik04@users.noreply.github.com>
2024-07-23 11:21:31 +0530

    Update dynamic = 'force-dynamic'

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import {authOptions} from "@/utils/authOptions"
 import NextAuth from "next-auth";
 const handler = NextAuth(authOptions);
-
+export const dynamic = 'force-dynamic'
 export { handler as GET, handler as POST };
-export const dynamicParams = true
