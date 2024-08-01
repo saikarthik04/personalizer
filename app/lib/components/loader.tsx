8e6b8e01b67c973df5ff31d11bee44dbbc8ad12a
saikarthik.mudigonda@techwave.net <saikarthik.mudigonda@techwave.net>
2024-08-01 13:23:38 +0530

    updated loader

--- app/lib/components/loader.tsx
+++ app/lib/components/loader.tsx
@@ -1,9 +1,18 @@
+import { DM_Serif_Display } from 'next/font/google';
+import Image from 'next/image';
 import React from 'react'
-
+// const dm_Serif_Display = DM_Serif_Display({
+//   weight: ["400"],
+//   preload: false,
+// });
 const Loader = () => {
   return (
-    <div className='md:text-8xl text-4xl font-semibold capitalize tracking-widest 
-      bg-gradient-to-br flex justify-center align-middle items-center text-center h-screen md:font-bold loaderBg text-emerald-950'>personalizer</div>
+    // <div className='md:text-8xl text-4xl font-semibold capitalize tracking-widest 
+    //   bg-gradient-to-br flex justify-center align-middle items-center text-center h-screen md:font-bold loaderBg text-emerald-950'> <span className={dm_Serif_Display.className}>personalizer</span></div>
+
+      <div className='flex justify-center align-middle items-center text-center h-screen bg-black'>
+        <Image src="/icons/brand-logo.svg" alt='brand-loader' width={120} height={120}></Image>
+      </div>
   )
 }
 
--- app/lib/components/navbar.tsx
+++ app/lib/components/navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { FaGithub } from "react-icons/fa6";
 import { Playfair_Display } from "next/font/google";
+import { DM_Serif_Display } from "next/font/google";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { RxHamburgerMenu } from "react-icons/rx";
 const playfair_Display = Playfair_Display({
@@ -10,79 +11,84 @@ const playfair_Display = Playfair_Display({
   preload: false,
   subsets: ["latin"],
 });
-import { SignOutButton } from "./buttons";
+const dm_Serif_Display = DM_Serif_Display({
+  weight: ["400"],
+  preload: false,
+});
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Settings from "./settings";
-// let clickHamburger : boolean =false
-// let menuItems=[];
-// const handleHamBuger = (event:any) => {
-//   clickHamburger = true
-//    menuItems=[
-//     {id:"0", item_name:"login"},
-//     {id:"1", item_name:"github"}
-//   ]
-// }
+
 interface NavbarProps {
   toggleSidebar?: () => void;
 }
 
-const Navbar = ({toggleSidebar}:NavbarProps) => {
+const Navbar = ({ toggleSidebar }: NavbarProps) => {
   const sessionData = useSession();
   const [showSetting, setSetting] = useState(false);
 
   console.log(sessionData, "nav");
   return (
     <>
-    <nav className=" border-b-2 border-white z-10 border-solid navbar-sticky">
-      <div className="h-20 flex md:ml-44 md:mr-40 mx-10 items-center md:justify-between text-center nav_pos">
-      <li className="md:hidden absolute left-6 items-center list-none ">
-         {sessionData.data?.user ?  
-         <>
-        {/* <Image
-          src={sessionData.data?.user?.image}
-          alt={sessionData.data?.user.name}
-          fill
-          onClick={toggleSidebar}
-          className="h-10 w-16 rounded-full flex md:hidden"
-        ></Image> */}
-        <RxHamburgerMenu className="w-6 h-6 mt-2 justify-center items-center md:hidden" onClick={toggleSidebar}></RxHamburgerMenu></>
-        : ("")}
-        </li> 
-        <a className="md:text-4xl font-thin text-3xl" href="/">
-          <h1 className={playfair_Display.className}>Personalizer</h1>
-        </a>
-        <div className="md:flex md:items-center md:list mt-2 float-right items-center">
-          <Link
-            href="https://github.com/saikarthik04/personalizer" 
-            className="pr-8 md:flex hidden"
-          >
-            <FaGithub className="text-3xl bg-black opacity-80 hover:opacity-100" />
-          </Link>
-          {sessionData.status == "authenticated" &&
-          sessionData.data.user != null ? (
-            <>
-            <Image
+      <nav className=" border-b-2 border-white z-10 border-solid navbar-sticky">
+        <div className="h-20 flex md:ml-44 md:mr-40 mx-10 items-center md:justify-between text-center nav_pos">
+          <li className="md:hidden absolute left-6 items-center list-none ">
+            {sessionData.data?.user ? (
+              <>
+                <RxHamburgerMenu
+                  className="w-6 h-6 mt-2 justify-center items-center md:hidden"
+                  onClick={toggleSidebar}
+                ></RxHamburgerMenu>
+              </>
+            ) : (
+              ""
+            )}
+          </li>
+          <li className="list-none flex flex-row items-center">
+          <a className="md:text-4xl font-thin text-3xl  contents" href="/">
+          <Image src="/icons/brand-logo.svg" alt="brand-logo" height={80} width={80}/>
+            <h1 className={dm_Serif_Display.className} >
+            rsonalizer</h1>
+          </a>
+          </li>
+          <div className="md:flex md:items-center md:list mt-2 float-right items-center">
+            <Link
+              href="https://github.com/saikarthik04/personalizer"
+              className="pr-8 md:flex hidden"
+            >
+              <FaGithub className="text-3xl bg-black opacity-80 hover:opacity-100" />
+            </Link>
+            {sessionData.status == "authenticated" &&
+            sessionData.data.user != null ? (
+              <>
+                <Image
                   src={sessionData.data?.user?.image}
                   alt={sessionData.data?.user.name}
                   width={30}
                   height={30}
                   className=" rounded-full flex"
-                  onClick={()=>setSetting(!showSetting)}
+                  onClick={() => setSetting(!showSetting)}
                 ></Image>
               </>
-          ) : (
-           <Link href="/login"><FaRegCircleUser className="h-11 w-9"/></Link>
-          )}
+            ) : (
+              <Link href="/login">
+                <FaRegCircleUser className="h-11 w-9" />
+              </Link>
+            )}
+          </div>
         </div>
-      </div>
-    </nav>
-    {showSetting ?
-    <Settings data={sessionData.data} status={sessionData.status} update={sessionData.update}  />
-    :('')
-}
+      </nav>
+      {showSetting ? (
+        <Settings
+          data={sessionData.data}
+          status={sessionData.status}
+          update={sessionData.update}
+        />
+      ) : (
+        ""
+      )}
     </>
   );
 };
 
-export default Navbar
+export default Navbar;
