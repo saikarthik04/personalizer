8dd8ed0e57b5e20a4c5c25fb8d0bed8f42d3d8ca
saikarthik.mudigonda@techwave.net <saikarthik.mudigonda@techwave.net>
2024-08-04 13:25:51 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import ClientSessionProvider from "./clientSessionProvider"; 
+import ClientSessionProvider from "@/app/ClientSessionProvider"; 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
